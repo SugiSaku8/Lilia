127d838c674f019f880682f1f0e077a83b4b620f
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2024-03-17 16:09:03 +0900

    Create cpu_gpu.c

---
+++ src/get/cpu_gpu.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_cpu_info() {
+    FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
+    if (cpuinfo == NULL) {
+        perror("fopen");
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), cpuinfo)) {
+        printf("%s", line);
+    }
+
+    fclose(cpuinfo);
+}
