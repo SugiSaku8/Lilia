739f05aab7e57b73793e2636400feca6743747fc
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2024-03-17 16:08:36 +0900

    Create sys.c

---
+++ src/get/sys.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/sysinfo.h>
+#include <sys/utsname.h>
+
+void print_system_info() {
+    struct utsname unameData;
+    struct sysinfo sysInfo;
+    if (uname(&unameData) != 0) {
+        perror("uname");
+        return;
+    }
+
+    printf("OS: %s %s %s\n", unameData.sysname, unameData.nodename, unameData.release);
+    if (sysinfo(&sysInfo) != 0) {
+        perror("sysinfo");
+        return;
+    }
+
+    printf("Total RAM: %lu\n", sysInfo.totalram);
+}
