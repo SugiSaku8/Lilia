6b32265939c3202a2865ea908b3f11d0dbf86843
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2024-03-17 16:09:22 +0900

    Create ram.c

---
+++ src/get/ram.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_memory_info() {
+    FILE *meminfo = fopen("/proc/meminfo", "r");
+    if (meminfo == NULL) {
+        perror("fopen");
+        return;
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), meminfo)) {
+        printf("%s", line);
+    }
+
+    fclose(meminfo);
+}
