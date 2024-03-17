f4a166f136f0c42e15c8195e5def4481bf7ebb8a
SugiSaku <155712184+SugiSaku8@users.noreply.github.com>
2024-03-17 16:17:41 +0900

    Update hardware_info.c

--- src/get/API/hardware_info.c
+++ src/get/API/hardware_info.c
@@ -1,7 +1,6 @@
-#include "hardware_info.h"
+#include "include/hardware_info.h"
 #include <stdio.h>
 
 void print_hardware_info() {
-    // ここでハードウェア情報を取得する処理を実装
     printf("Hardware information not implemented.\n");
 }
